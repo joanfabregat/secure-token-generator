8e5d60b923da040827dea363a09894ae87487ea4
Joan Fabrégat <joan@codeinc.co>
2024-05-14 11:41:25 +0200

    adds `allowUppercase` and `allowLowercase` options to `SecureTokenGenerator::generate()`

--- src/SecureTokenGenerator.php
+++ src/SecureTokenGenerator.php
@@ -18,16 +18,25 @@ final class SecureTokenGenerator
      * Generates a random token.
      *
      * @param int $length             The token length
-     * @param bool $allowDigits       If true, the token will contain digits
+     * @param bool $allowDigits       If true, the token will contain digits (0-9)
      * @param bool $allowSpecialChars If true, the token will contain special characters
+     *                                (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+     * @param bool $allowUppercase    If true, the token will contain uppercase letters (A-Z)
+     * @param bool $allowLowercase    If true, the token will contain lowercase letters (a-z)
      * @return string
-     * @throws RandomException
+     * @throws RandomException If the random generator fails
      */
-    public static function generate(int $length, bool $allowDigits = true, bool $allowSpecialChars = false): string
-    {
-        $key = '';
+    public static function generate(
+        int $length,
+        bool $allowDigits = true,
+        bool $allowSpecialChars = false,
+        bool $allowUppercase = true,
+        bool $allowLowercase = true
+    ): string {
+        $token = '';
 
-        while (strlen($key) < $length) {
+        while (strlen($token) < $length) {
+            // 1: uppercase, 2: lowercase, 3: uppercase, 4: lowercase, 5: digits, 6: special characters
             $charType = random_int(1, 6);
 
             // Skipping the character type if it's not allowed
@@ -37,8 +46,14 @@ final class SecureTokenGenerator
             if (!$allowSpecialChars && $charType == 6) {
                 continue;
             }
+            if (!$allowUppercase && in_array($charType, [1, 3])) {
+                continue;
+            }
+            if (!$allowLowercase && in_array($charType, [2, 4])) {
+                continue;
+            }
 
-            $key .= match ($charType) {
+            $token .= match ($charType) {
                 1, 3 => chr(random_int(65, 90)), // uppercase letters
                 2, 4 => chr(random_int(97, 122)), // lowercase letters
                 5 => chr(random_int(48, 57)), // digits
@@ -46,6 +61,7 @@ final class SecureTokenGenerator
             };
         }
 
-        return $key;
+        return $token;
+    }
     }
 }
